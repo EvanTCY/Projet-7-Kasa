947c25125b283034bf783c7bb63e9f460e4ccaef
Evan TCHING CHI YEN <heimanu.evan@gmail.com>
2023-06-26 23:27:32 +0200

    responsive du site, configuration du style des pages en mobile first avec mediaquery de SASS

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Main from "./components/Main";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import "./styles/_index.scss";
+import "./styles/_main.scss";
 
 ReactDOM.render(
     <React.StrictMode>
