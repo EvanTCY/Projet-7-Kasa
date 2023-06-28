da011f18d583dfd1fdae8855c4199fa5cdb1b16d
Evan TCHING CHI YEN <heimanu.evan@gmail.com>
2023-06-28 16:53:30 +0200

    responsive design, version très petit écran : < 360px

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,11 @@ import "../styles/_Banner.scss";
 function Banner({ image, className, children }) {
     return (
         <div className={className} role="banner">
-            <img className="banner__img" src={image} alt="bannière-accueil" />
+            <img
+                className="banner__img banner__img--aboutUs"
+                src={image}
+                alt="bannière-accueil"
+            />
             {children}
         </div>
     );
